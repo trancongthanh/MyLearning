348714a50a5391952dfd6b400f551758f5c24dc8
Tran Cong Thanh <trancongthanhbk03@gmail.com>
2019-04-19 03:48:16 +0700

    demo add controllers, view

---
+++ MyDemoLearning/MyDemoLearning/Controllers/HelloWorldController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyDemoLearning.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: HelloWorld
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // 
+        // GET: /HelloWo rld/Welcome/ 
+
+        public string Welcome(string name, int numTimes)
+        {
+            return HttpUtility.HtmlEncode("Hello " + name + ", NumTimes is: " + numTimes);
+        }
+    }
+}
